e1da6353b2661d3b0fc96926a53710dcdcf9075d
moradchalaby <moradchalaby@gmail.com>
2022-02-28 19:27:16 +0300

    as

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -43,7 +43,72 @@ class _MainPageState extends State<MainPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Padding(
+            ElevatedButton(
+                onPressed: () {
+                  pcontroller.jumpToPage(0);
+                  SimpleDrawer.deactivate("left");
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(CustomColors.siyah)),
+                child: Container(
+                  width: 110,
+                  height: 50,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Kelime',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: CustomColors.manaArafield,
+                        semanticLabel: 'MANA',
+                        textDirection: TextDirection.rtl,
+                      ),
+                    ],
+                  ),
+                )),
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  pcontroller.jumpToPage(1);
+                  SimpleDrawer.deactivate("left");
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(CustomColors.siyah)),
+                child: Container(
+                  height: 50,
+                  width: 110,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Kafiye ',
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: CustomColors.kelimeAra,
+                        semanticLabel: 'MANA',
+                        textDirection: TextDirection.rtl,
+                      ),
+                      Text(
+                        ' Mânâ',
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
+                  ),
+                )),
+            /*  Padding(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
                   onPressed: () {
@@ -68,12 +133,16 @@ class _MainPageState extends State<MainPage> {
                   style: ButtonStyle(
                       backgroundColor:
                           MaterialStateProperty.all<Color>(CustomColors.siyah)),
-                  child: Text(
-                    'Kafiye ve Mânâ',
-                    maxLines: 1,
-                    style: Theme.of(context).textTheme.headline5,
+                  child: Row(
+                    children: [
+                      Text(
+                        'Kafiye ve Mânâ',
+                        maxLines: 1,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
                   )),
-            ),
+            ), */
           ],
         ),
       ),
@@ -110,6 +179,12 @@ class _MainPageState extends State<MainPage> {
                             },
                             icon: Image.asset('assets/images/img3.png'),
                           )),
+                    ),
+                    Icon(
+                      Icons.search,
+                      color: CustomColors.kelimeAra,
+                      semanticLabel: 'MANA',
+                      textDirection: TextDirection.rtl,
                     ),
                     Expanded(
                       flex: 3,
@@ -219,6 +294,12 @@ class _MainPageState extends State<MainPage> {
                             icon: Image.asset('assets/images/img3.png'),
                           )),
                     ),
+                    Icon(
+                      Icons.search,
+                      color: CustomColors.kelimeAra,
+                      semanticLabel: 'MANA',
+                      textDirection: TextDirection.rtl,
+                    ),
                     Expanded(
                       flex: 1,
                       child: Theme(
@@ -246,6 +327,11 @@ class _MainPageState extends State<MainPage> {
                           },
                         ),
                       ),
+                    ),
+                    Icon(
+                      Icons.search,
+                      color: CustomColors.manaArafield,
+                      semanticLabel: 'MANA',
                     ),
                     Expanded(
                       flex: 1,
--- lib/widget/mana_ara/kelime.dart
+++ lib/widget/mana_ara/kelime.dart
@@ -1,5 +1,9 @@
+import 'dart:ui';
+
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:draggable_scrollbar/draggable_scrollbar.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive_sozluk_desktop/model/debouncer.dart';
@@ -16,8 +20,12 @@ class ListManaAraKelime extends StatefulWidget {
 }
 
 class _ListManaAraKelimeState extends State<ListManaAraKelime> {
+  ScrollAction contac = ScrollAction();
   ScrollController controller =
       ScrollController(initialScrollOffset: 0.0, keepScrollOffset: false);
+  ScrollController controller2 =
+      ScrollController(initialScrollOffset: 0.0, keepScrollOffset: false);
+
   bool sapkali = false;
   bool deyim = false;
   TextEditingController mcontroller = new TextEditingController();
@@ -33,6 +41,54 @@ class _ListManaAraKelimeState extends State<ListManaAraKelime> {
     getIt<ManaAraModel>().getMana();
   }
 
+  final FocusNode _focusNode = FocusNode();
+  void _handleKeyEvent(RawKeyEvent event) {
+    var offset = controller.offset;
+    print(offset);
+    if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+      setState(() {
+        if (kReleaseMode) {
+          controller.animateTo(offset - 27,
+              duration: Duration(milliseconds: 30), curve: Curves.ease);
+        } else {
+          controller.animateTo(offset - 27,
+              duration: Duration(milliseconds: 30), curve: Curves.ease);
+        }
+      });
+    } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+      setState(() {
+        if (kReleaseMode) {
+          controller.animateTo(offset + 27,
+              duration: Duration(milliseconds: 30), curve: Curves.ease);
+          kelimesec + 1;
+        } else {
+          controller.animateTo(offset + 27,
+              duration: Duration(milliseconds: 30), curve: Curves.ease);
+          kelimesec + 1;
+        }
+      });
+    } else if (event.logicalKey == LogicalKeyboardKey.pageUp) {
+      if (kReleaseMode) {
+        controller.animateTo(offset - 340,
+            duration: Duration(milliseconds: 30), curve: Curves.ease);
+        kelimesec - 18;
+      } else {
+        controller.animateTo(offset - 340,
+            duration: Duration(milliseconds: 30), curve: Curves.ease);
+        kelimesec - 18;
+      }
+    } else if (event.logicalKey == LogicalKeyboardKey.pageDown) {
+      if (kReleaseMode) {
+        controller.animateTo(offset + 340,
+            duration: Duration(milliseconds: 30), curve: Curves.ease);
+      } else {
+        controller.animateTo(offset + 340,
+            duration: Duration(milliseconds: 30), curve: Curves.ease);
+      }
+    }
+    ;
+  }
+
   @override
   void initState() {
     getIt
@@ -44,6 +100,8 @@ class _ListManaAraKelimeState extends State<ListManaAraKelime> {
 
   @override
   void dispose() {
+    _focusNode.dispose();
+
     getIt<ManaAraModel>().removeListener(update);
     super.dispose();
   }
@@ -52,20 +110,25 @@ class _ListManaAraKelimeState extends State<ListManaAraKelime> {
         deyim = getIt<ManaAraModel>().deyim,
         kelimeList = getIt<ManaAraModel>().item,
         kelimesec = 0,
-        controller.jumpTo
+        controller =
+            ScrollController(initialScrollOffset: 0.0, keepScrollOffset: false),
       });
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
-    return Container(
-      decoration: BoxDecoration(
-        color: CustomColors.renk4,
-      ),
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: [
-          /*   Padding(
+    return RawKeyboardListener(
+      onKey: _handleKeyEvent,
+      autofocus: true,
+      focusNode: _focusNode,
+      child: Container(
+        decoration: BoxDecoration(
+          color: CustomColors.renk4,
+        ),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            /*   Padding(
             padding: const EdgeInsets.only(left: 3, right: 3),
             child: new TextField(
               style: Theme.of(context).textTheme.headline3,
@@ -110,75 +173,89 @@ class _ListManaAraKelimeState extends State<ListManaAraKelime> {
           Image.asset(
             'assets/images/cizgi.png',
           ), */
-          Expanded(
-              child: ListView.separated(
-                  // emptyItemBuilder: (context, index) => Text('geleyor'),
-                  physics:
-                      ScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-                  itemCount: kelimeList.length,
-                  separatorBuilder: (context, index) {
-                    return new Container(
-                      padding: EdgeInsets.zero,
-                      margin: EdgeInsets.zero,
-                      height: 0.05,
-                      color: Colors.white,
-                    );
-                  },
-                  controller: controller,
-                  //itemCount: kelimeList.length,
+            Expanded(
+              child: RawScrollbar(
+                thumbColor: CustomColors.gri,
+                radius: Radius.circular(20),
+                controller: controller,
+                scrollbarOrientation: ScrollbarOrientation.right,
+                thickness: kelimeList.length < 5000 ? 10 : 0,
+                child: ScrollConfiguration(
+                  behavior: ScrollConfiguration.of(context).copyWith(
+                    scrollbars: false,
+                  ),
+                  child: ListView.separated(
+                      // emptyItemBuilder: (context, index) => Text('geleyor'),
+                      physics: ScrollPhysics(
+                          parent: AlwaysScrollableScrollPhysics()),
+                      itemCount: kelimeList.length,
+                      separatorBuilder: (context, index) {
+                        return new Container(
+                          padding: EdgeInsets.zero,
+                          margin: EdgeInsets.zero,
+                          height: 0.05,
+                          color: Colors.white,
+                        );
+                      },
+                      controller: controller,
+                      //itemCount: kelimeList.length,
 
-                  itemBuilder: (context, index) {
-                    if (kelimeList.length == 0) {
-                      return Center(
-                          child: CircularProgressIndicator(
-                        backgroundColor: CustomColors.renk5,
-                        valueColor:
-                            AlwaysStoppedAnimation<Color>(CustomColors.renk4),
-                      ));
-                    } else {
-                      return new TextButton(
-                        style: ButtonStyle(
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                                EdgeInsets.all(0)),
-                            overlayColor: MaterialStateProperty.all<Color>(
-                                  CustomColors.hover.withOpacity(0.5)),
-                              backgroundColor: kelimesec == index
-                                  ? MaterialStateProperty.all<Color>(
-                                      CustomColors.hover.withOpacity(0.4))
-                                : MaterialStateProperty.all<Color>(
-                                    CustomColors.renk5)),
-                        onPressed: () {
-                          getMana(context, kelimeList, index);
-                          setState(() {
-                            kelimesec = index;
-                          });
-                        },
-                        child: AutoSizeText(
-                          kelimeList[index].text,
-                          textAlign: TextAlign.justify,
-                          maxLines: 1,
-                          style: kelimeList[index].deyimid == 0
-                              ? Theme.of(context).textTheme.subtitle1
-                              : Theme.of(context).textTheme.subtitle2,
-                        ),
-                      );
-                      /*ListTile(
-                        onTap: () {},
-                        title: Container(
-                          width: 50,
-                          child: AutoSizeText(
-                            kelimeList[index].text,
-                            textAlign: TextAlign.start,
-                            maxLines: 1,
-                            style: kelimeList[index].deyimid == 0
-                                ? Theme.of(context).textTheme.headline1
-                                : Theme.of(context).textTheme.headline4,
-                          ),
-                        ),
-                      );  */
-                    }
-                  })),
-          /* InkWell(
+                      itemBuilder: (context, index) {
+                        if (kelimeList.length == 0) {
+                          return Center(
+                              child: CircularProgressIndicator(
+                            backgroundColor: CustomColors.renk5,
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                CustomColors.renk4),
+                          ));
+                        } else {
+                          return new TextButton(
+                            style: ButtonStyle(
+                                padding: MaterialStateProperty.all<EdgeInsets>(
+                                    EdgeInsets.all(0)),
+                                overlayColor: MaterialStateProperty.all<Color>(
+                                    CustomColors.hover.withOpacity(0.5)),
+                                backgroundColor: kelimesec == index
+                                    ? MaterialStateProperty.all<Color>(
+                                        CustomColors.hover.withOpacity(0.4))
+                                    : MaterialStateProperty.all<Color>(
+                                        CustomColors.renk5)),
+                            onPressed: () {
+                              getMana(context, kelimeList, index);
+                              setState(() {
+                                kelimesec = index;
+                              });
+                            },
+                            child: AutoSizeText(
+                              kelimeList[index].text,
+                              textAlign: TextAlign.justify,
+                              maxLines: 1,
+                              style: kelimeList[index].deyimid == 0
+                                  ? Theme.of(context).textTheme.subtitle1
+                                  : Theme.of(context).textTheme.subtitle2,
+                            ),
+                          );
+                          /*ListTile(
+                            onTap: () {},
+                            title: Container(
+                              width: 50,
+                              child: AutoSizeText(
+                                kelimeList[index].text,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                style: kelimeList[index].deyimid == 0
+                                    ? Theme.of(context).textTheme.headline1
+                                    : Theme.of(context).textTheme.headline4,
+                              ),
+                            ),
+                          );  */
+                        }
+                      }),
+                ),
+              ),
+            ),
+
+            /* InkWell(
             onTap: () {
               setState(() {
                 deyim = !deyim;
@@ -209,7 +286,8 @@ class _ListManaAraKelimeState extends State<ListManaAraKelime> {
                     ),
             ),
           ), */
-        ],
+          ],
+        ),
       ),
     );
   }
